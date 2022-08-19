eab97da37928d1b24fd813a9a3076978cfa91637
tatsupiko <identity2022@outlook.jp>
2022-08-19 14:24:02 +0900

    タイトルシーン、タイトルのアニメーション、フェードアウト、ボタン効果音の追加　

--- Assets/Yoshizawa/SceneLord.cs
+++ Assets/Yoshizawa/SceneLord.cs
@@ -6,9 +6,22 @@ using UnityEngine.SceneManagement;
 public class SceneLord : MonoBehaviour
 {
     [SerializeField] string _lordScene;
+    [SerializeField] float _time;
+    AudioSource _audio;
 
-    public void LordScene(string scenename)
+    void Start()
     {
-        SceneManager.LoadScene(scenename);
+        _audio = GetComponent<AudioSource>();
+    }
+    public void LordScene(string cor_name)
+    {
+        StartCoroutine(LordInterval(cor_name));
+    }
+
+    IEnumerator LordInterval(string sceneName)
+    {
+        _audio.Play();
+        yield return new WaitForSeconds(_time);
+        SceneManager.LoadScene(sceneName);
     }
 }
